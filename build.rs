21406d8535827c815297c477af81d984f1f20162
larsjarlvik <lars.jarlvik@gmail.com>
2020-12-08 22:23:11 +0700

    Render triangle

---
+++ build.rs
@@ -0,0 +1,33 @@
+use std::fs;
+use glob::glob;
+use std::process::Command;
+
+fn main() {
+    let output_dir = "src\\shaders-compiled";
+    let shader_paths = glob("./src/shaders/**/*")
+        .expect("Failed to list shaders!");
+
+    fs::create_dir_all(output_dir).expect("Failed to create output directory!");
+
+    for entry in shader_paths {
+        match entry {
+            Ok(path) => {
+                println!("Compiling {}", path.display().to_string().as_str());
+
+                Command::new("./tools/glslangValidator")
+                    .arg("-V")
+                    .arg(path.display().to_string().as_str())
+                    .arg("-o")
+                    .arg(path.display().to_string().replace("src\\shaders", output_dir) + ".spv")
+                    .status().unwrap();
+            }
+            Err(e) => {
+                println!("{}", e);
+            }
+        }
+    }
+
+    println!("DONE!");
+
+}
+
